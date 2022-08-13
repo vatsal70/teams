91dd3a85b7f76560ec4edddc08c8b33e514822fe
vatsal70 <vatsalvohera70@gmail.com>
2022-08-13 14:51:30 +0530

    sixth commit

--- teams/views.py
+++ teams/views.py
@@ -58,14 +58,4 @@ def logout_method(request):
 
 
 def homepage(request):
-    dataframe = pd.read_excel('static/excel_file/data_file.xlsx')
-    params = {}
-    if request.method == 'POST':
-        data = request.POST.get('btn')
-        dataframe_values = dataframe[dataframe[data]==1]['File Name'].values
-        params = {
-            'dataframe_values': dataframe_values,
-            'data': data,
-            'fetching_data': True,
-        }
-    return render(request, "frontend/demo.html", params)
+    return render(request, "frontend/demo.html")
